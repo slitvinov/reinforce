e16d67a2822bd5ff7284dcecf0bfe24bcfc85d8e
Sergey Litvinov <slitvinov@gmail.com>
2024-06-02 03:24:24 +0200

    up

--- 0.py
+++ 0.py
@@ -1,90 +1,65 @@
 import gymnasium
-import matplotlib.pyplot as plt
 import random
 import statistics
+import jax
 import math
 import numpy as np
 
-class Env:
-    def __init__(self):
-        self.E = 1.0
-        self.m = 1.0
-        self.k = 1.0
-        self.dt = 0.01
-        self.reset()
-
-    def reset(self):
-        self.x = 5
-        self.u = 0
-        self.t = 0
-        return self.state(), None
-
-    def step(self, action):
-        dt = self.dt
-        m = self.m
-        f = action
-        self.u += dt * (f - self.k * self.x) / m
-        self.x += self.u * dt
-        self.t += dt
-
-        state = self.state()
-        reward = self.reward()
-        done = self.done()
-        info = None
-        return state, reward, done, done, info
-
-    def state(self):
-        return np.array([self.x, self.u])
-
-    def reward(self):
-        return 1.0 / (1e-1 + abs(0.5 * self.m * self.u**2 - self.E))
-
-    def done(self):
-        return self.t >= 2 * np.sqrt(self.k/self.m)
-
-def policy(th, state):
-    mu = th[2] * state + th[1]
-    sigma = th[0]
+def policy(theta, state):
+    sigma = math.exp(theta[0])
+    mu = theta[1] + state @ theta[2:]
     return statistics.NormalDist(mu, sigma).samples(1)[0]
 
+def policy_mean(theta, state):
+    mu = theta[1] + state @ theta[2:]
+    return mu.item()
 
-def dlogpi_dth(th, action, state):
-    mu = th[2] * state + th[1]
-    sigma = th[0]
-    return \
-        -((sigma-mu+action)*(sigma+mu-action))/sigma**3, \
-        -(mu-action)/sigma**2, \
-        -(mu-action)*state/sigma**2
+def logpi(theta, action, state):
+    sigma = jax.numpy.exp(theta[0])
+    mu = theta[1] + state @ theta[2:]
+    return jax.scipy.stats.norm.logpdf(action, mu, sigma)
 
+def episod():
+    Return = 0
+    state, info = env.reset()
+    while True:
+        action = policy_mean(theta, state)
+        state, reward, terminated, truncated, info = env.step(action > 0)
+        Return += reward
+        if terminated or truncated:
+            break
+    return Return
 
-#env = gymnasium.make("CartPole-v1")
-env = Env()
-th = [0.1, 0.0, 0.0]
-alpha = 1e-6
+seed = gymnasium.utils.seeding.np_random()
+dlogpi_dtheta = jax.grad(logpi)
+env = gymnasium.make("CartPole-v1")
+random.seed(12345)
+gamma = 0.99
+n_states, = env.observation_space.shape
+theta = jax.numpy.zeros(n_states + 2, dtype=float)
+alpha = 0.001
 n_episod = 0
 while True:
     S = {}
     A = {}
     R = {}
     t = 0
-    state, info = env.reset()
+    state, info = env.reset(seed=12345)
     while True:
-        S[t] = state[0]
-        A[t] = action = policy(th, S[t])
-        state, reward, terminated, truncated, info = env.step(action)
+        S[t] = state
+        A[t] = action = policy(theta, S[t])
+        state, reward, terminated, truncated, info = env.step(action > 0)
         t += 1
         R[t] = reward
         if terminated or truncated:
             break
     T = t
     if n_episod > 0 and n_episod % 100 == 0:
-        Return = math.fsum(R[k] for k in range(1, T + 1))
-        print("% 8d %8.2f [%s]" % (n_episod, Return, th))
+        Return = statistics.fmean(episod() for i in range(100))
+        print("% 8d %8.2f %s" % (n_episod, Return, theta))
     for t in range(T):
-        G = math.fsum(R[k] for k in range(t + 1, T + 1))
-        dlog = dlogpi_dth(th, A[t], S[t])
-        s2 = th[0] * th[0]
-        th[0] += alpha * s2 * G * dlog[0]
-        th[1] += alpha * s2 * G * dlog[1]
-        th[2] += alpha * s2 * G * dlog[2]
+        G = statistics.fsum(gamma**(k - t - 1) * R[k]
+                            for k in range(t + 1, T + 1))
+        dlog = dlogpi_dtheta(theta, A[t], S[t])
+        theta += alpha * gamma**t * G * dlog
     n_episod += 1
--- 1.py
+++ 1.py
@@ -3,7 +3,6 @@ import os.path
 import numpy as np
 import scipy.stats as st
 import matplotlib.pyplot as plt
-from joblib import Parallel, delayed
 
 
 class ShortCorridor:
@@ -46,196 +45,58 @@ class ShortCorridor:
             return -1, state
 
 
-class ReinforceAgent:
-    """
-    A REINFORCE agent with a discrete policy parameterization and linear function approximation.
-    """
-
-    def __init__(self, num_actions, alpha):
-        self.num_actions = num_actions
-        self.alpha = alpha
-        # Initialize the policy parameters:
-        self.theta = np.log([[19], [1]])  # 5% chance of taking action 'right'
-
-    def pi(self, x_s):
-        """
-        Compute action probabilities from action preferences:
-        :param x_s: state feature vector
-        :return: an array of action probabilities
-        """
-        # Compute action preferences for the given feature vector:
-        preferences = self.theta.dot(x_s)
-        # Convert overflows to underflows:
-        preferences = preferences - preferences.max()
-        # Convert the preferences into probabilities:
-        exp_prefs = np.exp(preferences)
-        return exp_prefs / np.sum(exp_prefs)
-
-    def select_action(self, x_s):
-        return np.random.choice(2, p=self.pi(x_s).squeeze())
-
-    def eligibility_vector(self, a, s):
-        return self.x(s, a) - self.pi(self.x(s)) * (
-            self.x(s, ShortCorridor.left) + self.x(s, ShortCorridor.right))
-
-    def x(self, s, a=None):
-        """
-        Function approximator that computes state or state-action features.
-        """
-        if a is None:
-            return np.array([[1]])
-        elif a == ShortCorridor.right:
-            return np.array([[0], [1]])
-        elif a == ShortCorridor.left:
-            return np.array([[1], [0]])
-        else:
-            raise ValueError('Invalid Action!')
-
-    def learn(self, s_t, a_t, g_t):
-        # Get state features:
-        x_s = self.x(s_t)
-
-        # Update policy weights:
-        self.theta += self.alpha * g_t * self.eligibility_vector(a_t, s_t)
+def pi(x_s):
+    preferences = theta.dot(x_s)
+    preferences = preferences - preferences.max()
+    exp_prefs = np.exp(preferences)
+    return exp_prefs / np.sum(exp_prefs)
 
 
-def experiment(returns, alpha_index, alpha, run_num, random_seed, num_episodes,
-               max_timesteps):
-    np.random.seed(random_seed)
-    agent = ReinforceAgent(num_actions=ShortCorridor.num_actions, alpha=alpha)
-
-    for episode_num in range(num_episodes):
-        episode = []
-        g = 0.0
-        t = 0
-
-        # Start an episode:
-        s = ShortCorridor.init()
-        x_s = agent.x(s)
-
-        # Play out the episode:
-        while (s is not None) and (t < max_timesteps):
-            # Select action to take:
-            a = agent.select_action(x_s)
-
-            # Take action a, observe reward r' and next state s':
-            r_prime, s_prime = ShortCorridor.step(s, a)
-
-            # Save sequence for later:
-            episode.append((s, a, r_prime))
-
-            # Update counters:
-            s = s_prime
-            g = g + r_prime
-            t = t + 1
-
-        # Store returns:
-        returns[alpha_index, run_num, episode_num] = g
-
-        # Episode finished, so update the agent:
-        gt = g
-        for t in range(len(episode)):
-            # Unpack timestep:
-            s, a, r_prime = episode[t]
-
-            agent.learn(s, a, gt)
-
-            # Compute return from t until end of episode for next timestep:
-            gt = gt - r_prime
+def select_action(x_s):
+    return np.random.choice(2, p=pi(x_s).squeeze())
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-        description=
-        'A script to generate figure 13.1 from Sutton and Barto (2nd Ed.)')
-    parser.add_argument('--alphas',
-                        type=float,
-                        nargs='*',
-                        default=[2**-12, 2**-13, 2**-14],
-                        help='Policy step sizes')
-    parser.add_argument('--num_runs',
-                        type=int,
-                        default=100,
-                        help='The number of runs to average over')
-    parser.add_argument('--num_episodes',
-                        type=int,
-                        default=1000,
-                        help='The number of episodes per run')
-    parser.add_argument(
-        '--max_timesteps',
-        type=int,
-        default=1000,
-        help='The maximum number of timesteps allowed per episode')
-    parser.add_argument('--random_seed',
-                        type=int,
-                        default=2565,
-                        help='The random seed to use')
-    parser.add_argument('--num_cpus',
-                        type=int,
-                        default=-1,
-                        help='The number of cpus to use')
-    parser.add_argument('--confidence_intervals',
-                        action='store_true',
-                        help='Plot confidence intervals')
-    args = parser.parse_args()
+def eligibility_vector(a, s):
+    return x(
+        s,
+        a) - pi(x(s)) * (x(s, ShortCorridor.left) + x(s, ShortCorridor.right))
 
-    # Set the random seed:
-    np.random.seed(args.random_seed)
-    # Generate a random seed for each run:
-    random_seeds = [
-        np.random.randint(low=0, high=2**32) for run in range(args.num_runs)
-    ]
 
-    # If the data file already exists, use it instead of re-generating the data:
-    if os.path.exists('returns_13_1.npy'):
-        # Create memmapped arrays to be populated in parallel:
-        returns = np.memmap('returns_13_1.npy',
-                            shape=(len(args.alphas), args.num_runs,
-                                   args.num_episodes),
-                            dtype=np.int16,
-                            mode='r')
+def x(s, a=None):
+    if a is None:
+        return np.array([[1]])
+    elif a == ShortCorridor.right:
+        return np.array([[0], [1]])
+    elif a == ShortCorridor.left:
+        return np.array([[1], [0]])
     else:
-        # Create memmapped arrays to be populated in parallel:
-        returns = np.memmap('returns_13_1.npy',
-                            shape=(len(args.alphas), args.num_runs,
-                                   args.num_episodes),
-                            dtype=np.int16,
-                            mode='w+')
+        raise ValueError('Invalid Action!')
 
-        # Run experiments in parallel:
-        Parallel(n_jobs=args.num_cpus, verbose=10)(
-            delayed(
-                experiment)(returns, alpha_index, alpha, run_num, random_seed,
-                            args.num_episodes, args.max_timesteps)
-            for run_num, random_seed in enumerate(random_seeds)
-            for alpha_index, alpha in enumerate(args.alphas))
 
-    # Plot the results:
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    for alpha_index, alpha in enumerate(args.alphas):
-        # Average over runs:
-        means = np.mean(returns[alpha_index], axis=0)
-        p = plt.plot(
-            np.arange(args.num_episodes),
-            means,
-            label='2^{}'.format(int(np.log2(
-                alpha))))  # keep reference for colour-matching with errorbars.
-
-        if args.confidence_intervals:
-            # Plot 95% confidence intervals:
-            sems = st.sem(returns[alpha_index], axis=0)
-            confs = sems * st.t.ppf((1.0 + 0.95) / 2, args.num_runs - 1)
-            ax.errorbar(np.arange(args.num_episodes),
-                        means,
-                        yerr=[confs, confs],
-                        color=p[0].get_color(),
-                        alpha=.15)
-
-    ax.legend(title='Step size $\\alpha$:')
-    ax.set_xlabel('Episode')
-    ax.set_ylabel('Total reward on episode')
-    ax.set_ylim(-90, -10)
-    ax.set_title('Performance of REINFORCE (averaged over {} runs)'.format(
-        args.num_runs))
-    plt.savefig('figure_13_1.png')
+alpha = 2**-13
+max_timesteps = 1000
+theta = np.log([[19], [1]])
+num_episodes = 10000
+returns = []
+for episode_num in range(num_episodes):
+    episode = []
+    g = 0.0
+    t = 0
+    s = ShortCorridor.init()
+    x_s = x(s)
+    while s is not None and t < max_timesteps:
+        a = select_action(x_s)
+        r_prime, s_prime = ShortCorridor.step(s, a)
+        episode.append((s, a, r_prime))
+        s = s_prime
+        g = g + r_prime
+        t = t + 1
+    returns.append(g)
+    gt = g
+    for t in range(len(episode)):
+        s, a, r_prime = episode[t]
+        x_s = x(s)
+        ev = eligibility_vector(a, s)
+        theta += alpha * g * eligibility_vector(a, s)
+        gt = gt - r_prime
+print(returns[-10:])
